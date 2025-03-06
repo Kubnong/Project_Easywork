4e4a8cd48250834ef906a1b1f69117690426ac6f
Sandy <94830724+suksunarm@users.noreply.github.com>
2025-03-07 00:56:17 +0700

    add StartScreen, Register, and Login pages
    เพิ่มหน้าแรก สมัครสมาชิก และเข้าสู่ระบบ

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
-import { createStackNavigator } from "@react-navigation/stack"
+import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import React from "react"
-import StartScreen from "./src/screens/StartScreen"
+import React from "react";
+import StartScreen from "./src/screens/StartScreen";
+import Register from "./src/screens/Register";
+import Login from "./src/screens/Login";
+import LinearGradient from 'react-native-linear-gradient';
 
-const Stack = createStackNavigator()
-const App = () =>{
+const Stack = createStackNavigator();
+const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="StartScreen">
-        <Stack.Screen name="StartScreen" component={StartScreen}/>
+         <Stack.Navigator initialRouteName="StartScreen" screenOptions={{
+        headerStyle: {backgroundColor: "#77D499"},
+        headerTintColor: "#fff",
+        headerTitleAlign: "center",
+        headerTitleStyle: { fontSize : 25 , fontWeight: "bold" , color: "white"}
+      }}>
+      
+        <Stack.Screen name="StartScreen" component={StartScreen} options={{title:"" , headerStyle:{backgroundColor:"white"}}}/>
+        <Stack.Screen name="RegisterScreen" component={Register} options={{title:"สมัครสมาชิก"}}/>
+        <Stack.Screen name="LoginScreen" component={Login} options={{title:"เข้าสู่ระบบ"  , headerLeft: () => null}} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default App
+export default App;
