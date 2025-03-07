f8d2bd400e5447c31df728003da39142dc42e944
Kubnong <riw3566@gmail.com>
2025-03-07 17:50:26 +0700

    Update Profile Screens
    อัพเดต Profile Screens และปรับแต่งหน้า Mobile ต่าง ๆ ไม่ให้มีปุ่มย้อนกลับ

--- App.js
+++ App.js
@@ -4,15 +4,22 @@ import React from "react"
 import StartScreen from "./src/screens/StartScreen"
 import HomeScreen from "./src/screens/HomeScreen";
 import Profile from "./src/screens/Profile";
+import AddDetail from "./src/screens/AddDetail";
 
 const Stack = createStackNavigator()
 const App = () =>{
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomeScreen">
-        <Stack.Screen name="StartScreen" component={StartScreen}/>
-        <Stack.Screen name="HomeScreen" component={HomeScreen}/>
-        <Stack.Screen name="Profile" component={Profile}/>
+      <Stack.Navigator initialRouteName="AddDetail" screenOptions={{
+        headerStyle: {backgroundColor: "#77D499"},
+        headerTintColor: "#fff",
+        headerTitleAlign: "center",
+        headerTitleStyle: { fontSize : 25 , fontWeight: "bold" , color: "white"}
+      }}>
+        <Stack.Screen name="StartScreen" component={StartScreen} options={{ headerLeft: () => null }} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerLeft: () => null }} />
+        <Stack.Screen name="Profile" component={Profile} options={{ headerLeft: () => null }} />
+        <Stack.Screen name="AddDetail" component={AddDetail} />
       </Stack.Navigator>
     </NavigationContainer>
   )
