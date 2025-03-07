5847daf8e088eabad3fbd7d396d680b79f283b31
Sandy <94830724+suksunarm@users.noreply.github.com>
2025-03-07 18:07:07 +0700

    update App.js
    เพิ่มหน้า Register login RegisterFreelance

--- App.js
+++ App.js
@@ -5,20 +5,59 @@ import React from "react";
 import HomeScreen from "./src/screens/HomeScreen";
 import Profile from "./src/screens/Profile";
 import AddDetail from "./src/screens/AddDetail";
+import StartScreen from "./src/screens/StartScreen";
+import Register from "./src/screens/Register";
+import Login from "./src/screens/Login";
+import RegisFreelance from "./src/screens/RegisFreelance";
+import LinearGradient from "react-native-linear-gradient";
 
 const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="AddDetail" screenOptions={{
-        headerStyle: {backgroundColor: "#77D499"},
-        headerTintColor: "#fff",
-        headerTitleAlign: "center",
-        headerTitleStyle: { fontSize : 25 , fontWeight: "bold" , color: "white"}
-      }}>
-        <Stack.Screen name="StartScreen" component={StartScreen} options={{ headerLeft: () => null }} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerLeft: () => null }} />
-        <Stack.Screen name="Profile" component={Profile} options={{ headerLeft: () => null }} />
+      <Stack.Navigator
+        initialRouteName="RegisFreelanceScreen"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#77D499" },
+          headerTintColor: "#fff",
+          headerTitleAlign: "center",
+          headerTitleStyle: {
+            fontSize: 25,
+            fontWeight: "bold",
+            color: "white",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="RegisterScreen"
+          component={Register}
+          options={{ title: "สมัครสมาชิก" }}
+        />
+        <Stack.Screen
+          name="LoginScreen"
+          component={Login}
+          options={{ title: "เข้าสู่ระบบ", headerLeft: () => null }}
+        />
+        <Stack.Screen
+          name="RegisFreelanceScreen"
+          component={RegisFreelance}
+          options={{ title: "สมัครฟรีแลนซ์" }}
+        />
+        <Stack.Screen
+          name="StartScreen"
+          component={StartScreen}
+          options={{ headerLeft: () => null }}
+        />
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ headerLeft: () => null }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+          options={{ headerLeft: () => null }}
+        />
         <Stack.Screen name="AddDetail" component={AddDetail} />
       </Stack.Navigator>
     </NavigationContainer>
