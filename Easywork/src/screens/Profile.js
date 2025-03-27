ae4c3cfc35f324b888d1478d7d4f8235e525e13b
Sandy <94830724+suksunarm@users.noreply.github.com>
2025-03-28 01:39:30 +0700

    Add freelance detail column to Freelance table
    เพิ่มคอลัมน์รายละเอียดฟรีแลนซ์ในตารางฟรีแลนซ์ และสร้างไฟล์ Verify2.js

--- Easywork/src/screens/Profile.js
+++ Easywork/src/screens/Profile.js
@@ -34,7 +34,7 @@ const Profile = ({ navigation, setUserToken, userId }) => {
 
       try {
         const response = await axios.get(
-          `http://192.168.1.115:5000/profile/${userId}`
+          `http://172.20.10.7:5000/profile/${userId}`
         );
         setUser(response.data); // เก็บข้อมูลผู้ใช้ที่ดึงมา
       } catch (error) {
